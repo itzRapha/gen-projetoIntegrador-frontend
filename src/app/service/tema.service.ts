07bab08ac5eaa05c711bff6a272bab4ee76f89be
João Almeida <78218571+joao295@users.noreply.github.com>
2022-01-31 16:01:29 -0300

    @joao295 [add Method GetAllTema]

--- src/app/service/tema.service.ts
+++ src/app/service/tema.service.ts
@@ -15,24 +15,12 @@ export class TemaService {
     headers: new HttpHeaders().set('Authorization', environment.token)
   }
 
+
   getAllTema(): Observable<Tema[]>{
-    return this.http.get<Tema[]>('https://blogpessoalgustavogalli.herokuapp.com/tema', this.token)
+    return this.http.get<Tema[]>('https://gen-ti.herokuapp.com/tema', this.token)
   }
 
-  getByIdTema(id: number): Observable<Tema>{
-    return this.http.get<Tema>(`https://blogpessoalgustavogalli.herokuapp.com/tema/${id}`, this.token)
-  }
+  
 
-  postTema(tema: Tema): Observable<Tema>{
-    return this.http.post<Tema>('https://blogpessoalgustavogalli.herokuapp.com/tema', tema, this.token)
-  }
-
-  putTema(tema: Tema): Observable<Tema>{
-    return this.http.put<Tema>('https://blogpessoalgustavogalli.herokuapp.com/tema', tema, this.token)
-  }
-
-  deleteTema(id: number){ // não precisa de Observable pq não tá passando um objeto, só um id
-    return this.http.delete(`https://blogpessoalgustavogalli.herokuapp.com/tema/${id}`, this.token)
-  }
 
 }
