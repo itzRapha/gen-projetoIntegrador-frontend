c8c3a3c4eb2c169fc6618afda2035cd534771f76
Raphaella souza <raphaellaguerra@gmail.com>
2022-02-01 14:40:58 -0300

    @itzRapha [Task: add components edit]

--- src/app/edit/postagem-edit/postagem-edit.component.ts
+++ src/app/edit/postagem-edit/postagem-edit.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Postagem } from 'src/app/model/Postagem';
 import { PostagemService } from 'src/app/service/postagem.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-postagem-edit',
@@ -19,6 +20,23 @@ export class PostagemEditComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    let id = this.route.snapshot.params['id']
+    this.findByIdPostagem(id)
   }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp:Postagem)=>{
+      this.postage =resp
+    })
+  }
+
+  atualizar(){
+    
+  }
+
 
 }
