079f479d96c6204d2f51dec79eb26c2db5172fa3
CaioGabrielAlves <caio0401.ca@gmail.com>
2022-02-02 14:24:38 -0300

    @CaioGabrielAlves [Task: Alterando Bugs]

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -19,7 +19,7 @@ export class PostagemService {
   }
 
   getByIdPostagem(id: number): Observable<Postagem>{
-    return this.http.get<Postagem>(`https://gen-ti.herokuapp.com/postagem${id}`,this.token)
+    return this.http.get<Postagem>(`https://gen-ti.herokuapp.com/postagem/${id}`,this.token)
   }
 
   postPostagem(postagem: Postagem): Observable<Postagem>{
@@ -27,14 +27,11 @@ export class PostagemService {
   }
 
   putPostagem(postagem: Postagem):Observable<Postagem>{
-    return this.http.put<Postagem>('https://gen-ti.herokuapp.com/postagem/',postagem, this.token)
+    return this.http.put<Postagem>('https://gen-ti.herokuapp.com/postagem/update',postagem, this.token)
   }
 
   deletePostagem(id: number){
-    return this.http.delete<Postagem>(`https://gen-ti.herokuapp.com/postagem${id}`,this.token)
+    return this.http.delete<Postagem>(`https://gen-ti.herokuapp.com/postagem/remove/${id}`,this.token)
 
   }
-
-  
-
 }
